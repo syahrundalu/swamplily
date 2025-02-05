5e5b0cb4555074e1f8fa3077de23712277185dcc
syahrundalu <dalu.syahrun@gmail.com>
2025-02-05 18:32:42 +0700

    Update

--- js/script.js
+++ js/script.js
@@ -194,18 +194,6 @@ const backgroundSwiper = new Swiper('.background-swiper', {
     });
   }
 
-  window.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-
-    if (window.scrollY > 50) {
-        navbar.classList.add('scrolled');
-        navbar.classList.remove('transparent');
-    } else {
-        navbar.classList.add('transparent');
-        navbar.classList.remove('scrolled');
-    }
-});
-
 
 
 })(jQuery);
